83d6811c556dca469fcac3a9da097ce20fb1ce39
THEJAS-BK <thejasbk998@gmail.com>
2025-05-19 22:28:35 +0530

    day1

---
+++ Simon_multi/simon.js
@@ -0,0 +1,7 @@
+let homepageBtn = document.querySelector(".homepage-startbtn");
+let homepage = document.querySelector(".homepage");
+
+homepageBtn.addEventListener("click", () => {
+  console.log("hello");
+  homepage.style = "display:none";
+});
