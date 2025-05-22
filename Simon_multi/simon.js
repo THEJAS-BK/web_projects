6e6bb5d9f7fae66f80f0db01e232a7004a783f89
THEJAS-BK <thejasbk998@gmail.com>
2025-05-22 22:27:05 +0530

    day4

--- Simon_multi/simon.js
+++ Simon_multi/simon.js
@@ -1,7 +1,11 @@
-let homepageBtn = document.querySelector(".homepage-startbtn");
-let homepage = document.querySelector(".homepage");
+let player1 = document.querySelector("#player1");
+let player2 = document.querySelector("#player2");
+let start = document.querySelector(".homepage-startbtn");
 
-homepageBtn.addEventListener("click", () => {
-  console.log("hello");
-  homepage.style = "display:none";
+start.addEventListener("click", () => {
+  let player_name1 = player1.value;
+  let player_name2 = player2.value;
+  if (player_name1 != "" && player_name2 != "") {
+  }
 });
+let time = document.querySelectorAll(".time");
